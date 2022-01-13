2cf87e69e76df6f8aa549d03b59c84cbcaf2c6aa
Alexey Pesotsky <pesthedog@mail.ru>
2022-01-13 20:23:30 +0400

    Add is_strange field to view

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -17,6 +17,7 @@ def storage_information_view(request):
                 'who_entered': visit.passcard.owner_name,
                 'entered_at': localtime(visit.entered_at),
                 'duration': format_duration(visit.get_duration()),
+                'is_strange': visit.is_visit_long(),
             }
         )
     context = {
